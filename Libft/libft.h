662c523057a885afaf80630c5228ca66953a0fcc
Leandro Balla <leandro.balla@outlook.com>
2023-04-06 10:17:56 +0930

    4 06/04/2023

--- Libft/libft.h
+++ Libft/libft.h
@@ -44,7 +44,6 @@ char   *ft_strjoin(char const *s1, char const *s2);
 char   *ft_strtrim(char const *s1, char const *set);
 
 /*
-char   *ft_strchr(const char *s, int i);
 char   **ft_split(char const *str, char c);
 char   *ft_itoa(int n);
 char   *ft_strmapi(char const *s, char (*f)(unsigned int, char));
